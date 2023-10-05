08d9a814b7cf045dc071f375f27cf32f9204f398
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2023-10-05 17:25:09 +0200

    Update monorepo config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['next/core-web-vitals', '@packages/configs/.eslintrc'],
+  extends: ['next/core-web-vitals', '@packages/configs'],
   rules: {
     'import/no-anonymous-default-export': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
