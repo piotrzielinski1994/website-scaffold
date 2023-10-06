09cb160b4634537ce99d2a8f208e3513126af563
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2023-10-06 11:20:33 +0200

    Clean shared eslint config

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,16 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: [],
-  parserOptions: {},
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-  },
-  overrides: [],
-  settings: {},
-  ignorePatterns: ['node_modules', '**/*.js', '**/*.d.ts'],
+  ignorePatterns: ['node_modules'],
   rules: {
     'import/no-anonymous-default-export': 'off',
   },
