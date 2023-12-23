2368856a9ee2fa6fbb1470141f7865eab3359f6e
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2023-12-23 23:41:36 +0100

    Fix apiPageToPage function

--- frontend/src/cms/pages/pages.helpers.ts
+++ frontend/src/cms/pages/pages.helpers.ts
@@ -1,5 +1,10 @@
+import { objectEntries, objectFromEntries } from '@packages/helpers/entries/entries';
+import { Entry } from '@packages/helpers/entries/entries.types';
 import { ApiLanguage } from '@packages/types';
 import { ApiPage, Page, Pages } from '@packages/types/pages/pages.types';
+import { Seo } from '../content/seo/seo.types';
+import { apiBlockToBlock } from '../blocks/blocks.transformers';
+import { Block } from '@packages/types/blocks/blocks.types';
 
 export const takePagePaths = (pages: ApiPage[], pageId: ApiPage['id']): Page['path'] => {
   const page = pages.find((page) => page.id === pageId);
@@ -28,4 +33,35 @@ export const takePageByParentId = (
   parentId: Page['parentPage'],
 ): Page | undefined => {
   return Object.values(pages).find((page) => page.parentPage === parentId);
+};
+
+export const takePageSeo = (page: ApiPage, pathPerLanguage: Page['path']): Page['seo'] => {
+  const entries = objectEntries(pathPerLanguage).map(([lang, path]) => {
+    const apiSeo = page.translations.find((it) => it.languages_code === lang)!.seo;
+    const entry: Entry<Page['seo']> = [
+      lang,
+      {
+        url: path,
+        title: apiSeo.title,
+        description: apiSeo.description,
+        image: apiSeo.image,
+      },
+    ];
+    return entry;
+  });
+
+  return objectFromEntries<Record<ApiLanguage['code'], Seo>>(entries);
+};
+
+export const takePageContent = (page: ApiPage): Page['content'] => {
+  // TODO: Add blocks to CMS
+  return {
+    'en-US': [],
+    'pl-PL': [],
+  };
+  return objectFromEntries<Record<ApiLanguage['code'], Block[]>>(
+    page.translations.map((it) => {
+      return [it.languages_code, it.content.map(apiBlockToBlock)];
+    }),
+  );
 };
--- frontend/src/cms/pages/pages.transformers.ts
+++ frontend/src/cms/pages/pages.transformers.ts
@@ -1,50 +1,11 @@
 import { ApiPage, Page, Pages } from '@packages/types/pages/pages.types';
-import { apiBlockToBlock } from '../blocks/blocks.transformers';
-import { takePagePaths } from './pages.helpers';
-import { Seo } from '../content/seo/seo.types';
-import { ApiLanguage } from '@packages/types';
-import { objectEntries, objectFromEntries, Entry } from '@packages/helpers/entries/entries';
-
-const asd = (
-  languages: ApiPage['translations'][0]['languages_code'][],
-): Record<ApiPage['translations'][0]['languages_code'], Seo> => {
-  const zxc = languages.map((lang) => {
-    return [
-      lang,
-      {
-        url: '',
-        title: '',
-        description: '',
-        image: null,
-      },
-    ];
-  });
-
-  return Object.fromEntries(zxc);
-};
+import { takePageContent, takePagePaths, takePageSeo } from './pages.helpers';
 
 export const apiPageToPage = (pages: ApiPage[], pageId: ApiPage['id']): Page => {
   const page = pages.find((page) => page.id === pageId);
   if (!page) throw Error(`Page ${pageId} not found.`);
 
   const pathPerLanguage = takePagePaths(pages, pageId);
-
-  const zxc = objectFromEntries<Record<ApiLanguage['code'], Seo>>(
-    objectEntries(pathPerLanguage).map(([lang, path]) => {
-      const entry: Entry<Page['seo']> = [
-        lang,
-        {
-          url: path,
-          title: '',
-          description: '',
-          image: '',
-        },
-      ];
-      return entry;
-    }),
-  );
-
-  console.log('@@@ zxc | ', zxc);
 
   return {
     id: page.id,
@@ -55,11 +16,8 @@ export const apiPageToPage = (pages: ApiPage[], pageId: ApiPage['id']): Page => 
     menus: page.menus,
     layout: page.layout,
     path: pathPerLanguage,
-    seo: zxc,
-    // title: page.title,
-    // description: page.description,
-    // image: page.image,
-    // content: page.content.map(apiBlockToBlock),
+    seo: takePageSeo(page, pathPerLanguage),
+    content: takePageContent(page),
     createdAt: new Date(page.created_at),
     updatedAt: page.updated_at ? new Date(page.updated_at) : null,
   };
